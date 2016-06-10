d829b52cfad442bc40f190aba4219d601f46d43f
Tbecton <tbecton@kippnyccp.org>
2016-06-10 12:26:59 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
- $("p").click(function(){
+ $("top").click(function(){
     alert("The border was clicked.");
 });
  document.#id "top" ("myDiv").style.border = "thin dotted Green";
